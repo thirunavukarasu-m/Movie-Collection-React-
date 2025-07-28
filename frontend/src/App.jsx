29a63e37bf9755da1fa160d3ec070c6f3914d596
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-07-28 17:15:41 +0530

    Added routing and navbar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import MovieCard from './components/MovieCard'
 import HomePage from './pages/HomePage'
+import Favorites from './pages/Favorites'
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <HomePage />
-      </div>
-    </>
+
+    <div className="">
+      <NavBar />
+    
+        <main className='main-content'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Routes>
+        </main>
+  </div>
   )
 }
 
