6888a4a299aacfe81afd06e1ba62b018abaaf150
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-07-28 18:31:45 +0530

    Modified the favorites page.

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import '../css/Favorites.css'
+import React from "react";
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 const Favorites = () => {
-  return (
-    <div className='favorites-empty'>
+  const { favorites } = useMovieContext();
+
+  if (!favorites) {
+    return (
+      <div className="favorites-empty">
         <h2>No Favorites yet.</h2>
         <p>Start adding some movies to your favorites list!</p>
-    </div>
-  )
-}
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className="favorites">
+          <h2>Your Favorites</h2>
+        </div>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard key={movie.title} movie={movie} />
+          ))}
+        </div>
+      </>
+    );
+  }
+};
 
-export default Favorites
+export default Favorites;
